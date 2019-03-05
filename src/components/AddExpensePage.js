5d6e2483ca1a023b6fb6d318152d12a67ae4cbb9
wooojek <yokomura123@gmail.com>
2019-03-05 23:28:53 +0100

    Add Expensify styles

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -4,21 +4,27 @@ import ExpenseForm from './ExpenseForm';
 import { startAddExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
-    onSubmit = (expense) => {
-        this.props.startAddExpense(expense);
-        this.props.history.push('/');
-    };
+  onSubmit = (expense) => {
+    this.props.startAddExpense(expense);
+    this.props.history.push('/');
+  };
 
-    render() {
-        return (
-            <div>
-                <h1>Add Expense</h1>
-                <ExpenseForm 
-                    onSubmit={this.onSubmit}
-                />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">Add Expense</h1>
+          </div>
+        </div>
+        <div className="content-container">
+          <ExpenseForm
+            onSubmit={this.onSubmit}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapDispatchToProps = (dispatch) => ({
